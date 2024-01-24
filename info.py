5efb265aec9ea38cf6bfbc6b84a49a4e945d3b9e
Christian Wiedemann-Gruber <christian.wiedemann@capgemini.com>
2024-01-24 17:41:32 +0100

    fast

--- info.py
+++ info.py
@@ -97,37 +97,37 @@ while True:
     draw.text((x, top+8),     str("F    "), font=font, fill=200)
     disp.image(image)
     disp.show()
-    time.sleep(2)
+    time.sleep(0.2)
 
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     draw.text((x, top+8),     str(" E   "), font=font, fill=200)
     disp.image(image)
     disp.show()
-    time.sleep(2)
+    time.sleep(0.2)
 
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     draw.text((x, top+8),     str("  L  "), font=font, fill=200)
     disp.image(image)
     disp.show()
-    time.sleep(2)
+    time.sleep(0.2)
 
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     draw.text((x, top+8),     str("   I "), font=font, fill=200)
     disp.image(image)
     disp.show()
-    time.sleep(2)
+    time.sleep(0.2)
 
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     draw.text((x, top+8),     str("    X"), font=font, fill=200)
     disp.image(image)
     disp.show()
-    time.sleep(2)
+    time.sleep(0.2)
 
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     draw.text((x, top+8),     str("FELIX"), font=font, fill=200)
     disp.image(image)
     disp.show()
-    time.sleep(2)
+    time.sleep(0.2)
 #     cmd = "top -bn1 | grep load | awk " \
 #           "'{printf \"CPU Load: %.2f\", $(NF-2)}'"
 #     CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
